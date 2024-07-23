cb64ce200bedd422d45ef9914fc2e94463c5ac4f
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-07-22 20:22:43 -0400

    Create file structure & User seed method

--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,43 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User, Search, Comment, Post
+
+def create_users():
+    users = []
+    usernames = [fake.unique.user_name() for i in range(20)]
+
+    for x in range(20):
+        user = User(
+            username = usernames[x],
+            password_hash = 'password'
+        )
+
+        users.append(user)
+    
+    return users
+
+def create_searches():
+    pass
+
+def create_comments():
+    pass
+
+def create_posts():
+    pass
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
-        print("Starting seed...")
-        # Seed code goes here!
+        print('Clearing db...')
+        User.query.delete()
+        Search.query.delete()
+        Comment.query.delete()
+        Post.query.delete()
+
+        print("Seeding users...")
+        users = create_users()
+        db.session.add_all(users)
+        db.session.commit()        
+        
+
