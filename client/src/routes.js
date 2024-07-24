7453b86eed836bfe94faa5cbb49e53874433c1b1
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-07-24 16:27:46 -0400

    Add Login route

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,11 +1,15 @@
 import App from './components/App'
+import Login from './pages/Login'
 
 const routes = [
     {
         path: '/',
         element: <App />,
         children: [
-
+            {
+                path: '/login',
+                element: <Login />
+            }
         ]
     }
 ]
