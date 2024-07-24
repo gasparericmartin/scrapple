3625bd861865e4f4fde70948473db205a3ebbe4e
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-07-24 16:26:20 -0400

    Create and populate Login page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,78 @@
+import {useState} from 'react'
+import {Formik, Form, Field} from 'formik'
+import * as Yup from 'yup'
+
+function Login() {
+    const [error, setError] = useState(false)
+
+    function handleSubmit(values) {
+        
+        const postObj = {
+            username: values.username,
+            password: values.password
+        }
+        
+        fetch('/login', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(postObj)
+        })
+        .then(r => {
+            if(r.ok) {
+                console.log('logged in')
+            }
+            else {
+                r.json()
+                .then((errorObj) => setError(errorObj.error))
+            }
+        })
+    }
+    
+    
+    return (
+        <Formik
+            validateOnChange={false}
+            validateOnBlur={false}
+            initialValues={{
+                username: '',
+                password: ''
+            }}
+            validationSchema={Yup.object().shape({
+                username: Yup.string()
+                    .max(32, 'Usernames must be 32 characters max')
+                    .required('Required'),
+                    password: Yup.string()
+                        .max(128, 'Passwords are 128 characters max')
+                        .required('Required')
+            })}
+            onSubmit={(values) => {
+                handleSubmit(values)
+            }}
+        >
+            {({errors}) => (
+                <Form>
+                    <label htmlFor='username'>Username</label>
+                    <Field 
+                        name='username' 
+                        type='text' 
+                        className='input input-bordered w-full max-w-xs'/>
+                    {errors.username ? <p>{errors.username}</p>: null}
+
+                    <label htmlfor='password'>Password</label>
+                    <Field 
+                        name='password'
+                        type='text'
+                        className='input input-bordered w-full max-2-xs'/>
+                    {errors.password ? <p>{errors.password}</p>: null}
+
+                    <button type='submit>' className='btn btn-primary'>Submit</button>
+                    {error ? <h2>{error}</h2>: null}
+                </Form>
+            )}
+        </Formik>
+    )
+}
+
+export default Login
