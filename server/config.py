9a7bb5599c1b173949c7149e89bca0f7172f3866
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-07-23 21:46:25 -0400

    Add flask Login manager to config

--- server/config.py
+++ server/config.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+import flask_login
 
 # Local imports
 
@@ -29,3 +30,7 @@ api = Api(app)
 
 # Instantiate CORS
 CORS(app)
+
+# Flask Login
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
